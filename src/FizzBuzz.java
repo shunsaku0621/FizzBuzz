2c14d33ca56f0586453a51a3f21001643095c755
shunsaku0621 <hamagi.0621@icloud.com>
2022-04-06 20:24:17 +0900

    課題再提出-リファクタリング

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,14 +3,14 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		for(int i = 1; i<=100; i++) {
-			if(i % 3==0 && i% 5==0) {
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz");
-			}else if(i % 3==0) {
+			} else if (i % 3 == 0) {
 				System.out.println("Fizz");
-			}else if(i% 5==0) {
+			} else if (i % 5 == 0) {
 				System.out.println("Buzz");
-			}else {
+			} else {
 				System.out.println(i);
 			}
 		}
